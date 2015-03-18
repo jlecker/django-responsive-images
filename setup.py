994cc8e847e75e991f77555e028a3feb39b68989
James Lecker <james@jameslecker.com>
2015-03-18 14:29:52 -0700

    add README as long_description in setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from distutils.core import setup
 import responsive_images
 
 
+with open('README.rst') as readme:
+    long_desc = readme.read()
+
 setup(
     name='django-responsive-images',
     version=responsive_images.__version__,
@@ -10,6 +13,7 @@ setup(
     author_email='james@convectivesoftware.com',
     url='https://github.com/ConvectiveSoftware/django-responsive-images',
     description='An Django app for resizing images and generating src and srcset values via template tags.',
+    long_description = long_desc,
     download_url='https://github.com/ConvectiveSoftware/django-responsive-images/tarball/{}'.format(responsive_images.__version__),
     packages=['responsive_images', 'responsive_images.templatetags'],
     classifiers=[
